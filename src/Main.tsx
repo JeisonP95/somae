f954836a5c7e41c3516577b68764d5f9c7fb7633
Yeison Pencue <168775486+JeisonP95@users.noreply.github.com>
2025-11-27 12:30:51 -0500

    Fix main.tsx name case

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './globals.css'
 import App from './App'
-
 // Establecer el título y la descripción de la página
 document.title = "SOMAÉ - Bienestar Femenino & Autocuidado"
 const metaDescription = document.querySelector('meta[name="description"]')
